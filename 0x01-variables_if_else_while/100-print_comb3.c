ba9611d852eec6a77ac2c0cdc6b7457c1e08e62d
razorrr96 <eng.abdelrahman.rahal@gmail.com>
2023-06-17 17:28:03 -0700

    test3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point test2
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n, m;
+
+	for (n = 48; n < 57; n++)
+	{
+		for (m = 49; m <= 57; m++)
+		{
+			if (n != m)
+			{
+				putchar(n);
+				putchar(m);
+				if (n == 56 && m == 57)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
