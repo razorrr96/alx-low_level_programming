e1d4063552db3ed757d07eeaa243f691b1bc51f9
razorrr96 <eng.abdelrahman.rahal@gmail.com>
2023-06-20 01:28:58 -0700

    test

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point test3
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
