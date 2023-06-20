a243e0d0a29e4ad2d958a8153965280845bed04f
razorrr96 <eng.abdelrahman.rahal@gmail.com>
2023-06-20 03:28:12 -0700

    test

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
- * main - Entry point test3
+ * main - print _putchar
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-putchar('_');
-putchar('p');
-putchar('u');
-putchar('t');
-putchar('c');
-putchar('h');
-putchar('a');
-putchar('r');
-putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
