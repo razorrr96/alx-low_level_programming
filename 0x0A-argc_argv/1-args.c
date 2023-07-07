0b6d87b6cbcd15f6b5abfc50c91662309aebfb11
razorrr96 <eng.abdelrahman.rahal@gmail.com>
2023-07-07 12:36:48 -0700

    test1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,9 +8,11 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(int argc, char *argv[])
 {
 	(void) argv;
 	printf("%d\n", argc - 1);
+
 	return(0);
 }
--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -17,5 +17,6 @@ int main(int argc, char *argv[])
 	{
 		printf("%s\n", argv[i]);
 	}
+
 	return(0);
 }
